66fa73e3555a1d20541d6fe1046c5236a9a0ec86
SeraLee <cieroyou1@gmail.com>
2020-11-22 21:08:06 +0900

    test: Restaurant에 Id 속성 추가함에 따라 관련 test 코드 수정

--- eatgo-api/src/test/java/kr/co/fastcompus/eatgo/domain/RestaurantTests.java
+++ eatgo-api/src/test/java/kr/co/fastcompus/eatgo/domain/RestaurantTests.java
@@ -8,14 +8,15 @@ class RestaurantTests {
 
     @Test
     public void creation() {
-        Restaurant restaurant = new Restaurant("Bob zip" , "Seoul");
+        Restaurant restaurant = new Restaurant(1004l, "Bob zip", "Seoul");
         assertEquals(restaurant.getName(), "Bob zip");
         assertEquals(restaurant.getAddress(), "Seoul");
+        assertEquals(restaurant.getId(), 1004l);
     }
 
     @Test
     public void information() {
-        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        Restaurant restaurant = new Restaurant(1004l, "Bob zip", "Seoul");
         assertEquals(restaurant.getInformation(), "Bob zip in Seoul");
     }
 
