27088190561453e72a0bcbfd48888208a98928fd
SeraLee <cieroyou1@gmail.com>
2020-11-22 19:57:28 +0900

    Hello World

---
+++ eatgo-api/src/main/java/kr/co/fastcompus/eatgo/domain/Restaurant.java
@@ -0,0 +1,23 @@
+package kr.co.fastcompus.eatgo.domain;
+
+public class Restaurant {
+    private final String name;
+    private String address;
+
+    public Restaurant(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public String getInformation() {
+        return name + " in " + address;
+    }
+}
---
+++ eatgo-api/src/test/java/kr/co/fastcompus/eatgo/domain/RestaurantTests.java
@@ -0,0 +1,23 @@
+package kr.co.fastcompus.eatgo.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantTests {
+
+    @Test
+    public void creation() {
+        Restaurant restaurant = new Restaurant("Bob zip" , "Seoul");
+        assertEquals(restaurant.getName(), "Bob zip");
+        assertEquals(restaurant.getAddress(), "Seoul");
+    }
+
+    @Test
+    public void information() {
+        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        assertEquals(restaurant.getInformation(), "Bob zip in Seoul");
+    }
+
+
+}
